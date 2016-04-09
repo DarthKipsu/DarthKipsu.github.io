92abdbbcea8dc3693fc907b6fc8de7a5138d28f8
Verna Koskinen <darth.kipsu@gmail.com>
2016-04-09 22:40:40 +0300

    fix screen scroll location for small screen sizes

--- script/layout.js
+++ script/layout.js
@@ -2,8 +2,9 @@
 function changeHeaderImgSize() {
     var img = document.getElementById('main-img')
     img.style.marginTop = '0'
+    console.log(img.height)
     
-    window.scroll(0, 460)
+    window.scroll(0, img.height - 60)
 }
 
 changeHeaderImgSize()
