44439d6eb6b5b2a946cde4b6fb6403058edeb5c9
Verna Koskinen <darth.kipsu@gmail.com>
2016-04-10 19:16:46 +0300

    use document element instead of page offset to get scroll location

--- script/layout.js
+++ script/layout.js
@@ -3,7 +3,7 @@ function changeHeaderImgSize() {
     var img = document.getElementById('main-img')
     img.style.marginTop = '0'
     
-    if (window.pageYOffset < img.height - 60) {
+    if (document.documentElement.scrollTop < img.height - 60) {
         window.scroll(0, img.height - 60)
     }
 }
